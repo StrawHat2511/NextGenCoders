f6f804729dd4923ca56c18c39283ff75a6357f76
Daniela <danielaguzman50@outlook.es>
2025-11-29 13:40:42 -0500

    rutas

--- public/js/header.js
+++ public/js/header.js
@@ -1,6 +1,6 @@
 // Cargar el header a la vista
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("./components/Header/Header.html")
+  fetch("../../components/Header/Header.html")
     .then((res) => res.text())
     .then((data) => {
       document.getElementById("header").innerHTML = data;
